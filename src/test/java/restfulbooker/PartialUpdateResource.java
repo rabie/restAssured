cb6367afa8df2a93b51d79732de497d6737259e0
Rabie El ouargua <rabie.elouargua@gmail.com>
2021-08-30 17:52:01 +0300

    Third commit

--- src/test/java/restfulbooker/PartialUpdateResource.java
+++ src/test/java/restfulbooker/PartialUpdateResource.java
@@ -1,4 +1,24 @@
 package restfulbooker;
 
+import io.restassured.RestAssured;
+
 public class PartialUpdateResource {
+    public static void main(String[] args) {
+        RestAssured
+                .given()
+                    .log().all()
+                    .header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=")
+                    .baseUri("https://restful-booker.herokuapp.com/")
+                    .basePath("booking/{id}")
+                    .pathParam("id", 6)
+                    .body("{\n" +
+                            "    \"firstname\" : \"James\",\n" +
+                            "    \"lastname\" : \"Brown\"\n" +
+                            "}")
+                .when()
+                    .patch()
+                .then()
+                    .log().all()
+                    .statusCode(200);
+    }
 }
